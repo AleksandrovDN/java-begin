4bdc16272dc0ccdf876a24d53eee11ddb3c37393
Дмитрий Николаевич Александров <61863007+AleksandrovDN@users.noreply.github.com>
2020-03-18 17:19:14 +0300

    Создание папок в Тестиуме

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Testosiym.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Testosiym.java
@@ -1,14 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+import java.io.File;
+
 public class Testosiym {
 
-    public static void main (String[] args) {
-        System.out.println("TESTTEST");
-        System.out.println("TESTTEST");
-        System.out.println("TESTTEST" +
-                "SWWDSLIJDWDWSLHJ" +
-                "ALWDJAOJIOWJAODJ" +
-                "OIWDFJOAWIDJOAJDAO");
+    public static void main(String[] args) {
 
+        // определяем объект для каталога
+        File dir = new File("E:/Devel/NewDir");
+        boolean created = dir.mkdir();
+        if(created)
+            System.out.println("Folder has been created");
+        // переименуем каталог
+       /* File newDir = new File("C://SomeDir//NewDirRenamed");
+        dir.renameTo(newDir);
+        // удалим каталог
+        boolean deleted = newDir.delete();
+        if(deleted)
+            System.out.println("Folder has been deleted");*/
     }
 }
